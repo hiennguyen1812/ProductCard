74908297dcb3544f65e427defa87bd7fe050f968
hien <77673551+hiennguyen1812@users.noreply.github.com>
2022-03-27 13:49:27 +0700

    1

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import Product_Card from "./Product_Card"
+import Card from "./Card";
+import Price from "./Price"
+
+function App() {
+  return (
+    <div className="App">
+      <Product_Card />
+      <Card/>
+      <Price/>
+    </div>
+  );
+}
+
+export default App;
